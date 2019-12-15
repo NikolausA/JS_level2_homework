2d676677db043f1a0a5368216e251632b7755cec
NikolausA <nik.aleksenko@gmail.com>
2019-12-15 15:59:41 +0300

    исправил ошибки после проверки ДЗ

--- index.js
+++ index.js
@@ -5,13 +5,12 @@ const goods = [
     { title: 'Shoes', price: 150 },
 ];
 
-const renderGoodsItem = (title, price) => {
-    return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`
-};
+const renderGoodsItem = (title = 'Без имени', price = '') =>
+`<div class="goods-item"><h3 class="title">${title}</h3><p>${price}</p></div>`;
 
-const renderGoodsList = (list) => {
+const renderGoodsList = (list = []) => {
     const goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-    document.querySelector('.goods-list').innerHTML = goodsList;
+    document.querySelector('.goods-list').innerHTML = goodsList.join('');
 };
 
 renderGoodsList(goods);
