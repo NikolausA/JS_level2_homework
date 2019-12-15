103e71b72ec81dafaaaf9b40e5bd837c4fbc01fb
NikolausA <nik.aleksenko@gmail.com>
2019-12-15 23:29:22 +0300

    перенес код второго урока в один js файл

--- index.js
+++ index.js
@@ -1,16 +1,62 @@
-const goods = [
-    { title: 'Shirt', price: 150 },
-    { title: 'Socks', price: 150 },
-    { title: 'Jacket', price: 150 },
-    { title: 'Shoes', price: 150 },
-];
+class GoodsItem {
+    constructor(title = 'Без имени', price = '') {
+        this.title = title;
+        this.price = price;
+    }
+    render() {
+        return `<div class="goods-item">
+                    <h3 class="title goods-title">${this.title}</h3>
+                    <p>${this.price} ₽</p>
+                </div>`;
+    }
+}
 
-const renderGoodsItem = (title = 'Без имени', price = '') =>
-`<div class="goods-item"><h3 class="title">${title}</h3><p>${price}</p></div>`;
+class GoodsList {
+    constructor() {
+        this.goods = [];
+    }
+    fetchGoods()  {
+        this.goods = [
+            { title: 'Shirt', price: 150 },
+            { title: 'Socks', price: 150 },
+            { title: 'Jacket', price: 150 },
+            { title: 'Shoes', price: 150 },
+        ];
+    }
+    render() {
+        let listHtml = '';
+        this.goods.forEach(good => {
+            const goodItem = new GoodsItem(good.title, good.price);
+            listHtml += goodItem.render();
+        });
+        document.querySelector('.goods-list').innerHTML = listHtml;
+    }
+    sumGoodsPrices() {
+        return this.goods.reduce((sum, good) => { 
+        if (good.price) sum += good.price; 
+        return sum;
+        }, 0);
+    }
+}
 
-const renderGoodsList = (list = []) => {
-    const goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-    document.querySelector('.goods-list').innerHTML = goodsList.join('');
-};
+class CartItem extends GoodsItem {
+    constructor(props) {
+        super(props);
+    }
+    delete() {}
+}
 
-renderGoodsList(goods);
+class Cart extends GoodsList {
+    constructor(props) {
+        super(props);
+    }
+    clean() {}
+    incGood() {}
+    decGood() {}
+}
+
+const list = new GoodsList();
+list.fetchGoods();
+list.render();
+list.sumGoodsPrices();
+console.log(list.sumGoodsPrices());
